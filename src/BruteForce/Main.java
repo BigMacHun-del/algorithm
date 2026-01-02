e0567ef6106dbb91d4da8fad4c3e45037d9d144b
김대훈 <eogns136@gmail.com>
2026-01-02 10:35:49 +0900

    Feat: 완전 탐색 연습 문제 구현 완료

--- src/BruteForce/Main.java
+++ src/BruteForce/Main.java
@@ -1,4 +1,6 @@
-import java.util.ArrayList;
+package BruteForce;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +9,7 @@ public class Main {
         int target = 9;
 
         int[] result = findPairs(numbers, target);
-
+        System.out.println(Arrays.toString(result));
 //        int count = 0;
 //        ArrayList<Integer> list1 = new ArrayList();
 //        ArrayList<Integer> list2 = new ArrayList();
@@ -26,6 +28,7 @@ public class Main {
 //        }
     }
 
+    //완전 탐색
     private static int[] findPairs(int[] numbers, int target) {
         //1. 배열 순회해서 첫 번째 수 선택
         for (int i=0; i<numbers.length; i++){
@@ -35,8 +38,17 @@ public class Main {
             //2. 두 번째 수 선택 및 검증 단계
             //2.1 첫 번째 수 다음 인덱스부터 배열 끝까지 순회해서 선택
             for (int j=0; i<numbers.length; j++){
-                int num2 = number[j];
+                //2.2 현재 선택도니 두 수의 합이 target과 같은지 확인
+                int num2 = numbers[j]; // 두 번째 수 선택
+                if(num1 + num2 == target){
+                    //2.3 target과 같다면 두 수의 인덱스 반환
+                    return new int[]{i,j};
+                }
             }
         }
+        //3. 결과 반환 단계
+        //3.1 조합을 찾지 못한 경우 빈 배열을 반환
+        return new  int[]{};
     }
+
 }
